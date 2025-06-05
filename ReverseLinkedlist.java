5c0911efae10994527c6e6efa72bfd1c3061046d
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-05 15:10:24 +0530

    Add files via upload

---
+++ ReverseLinkedlist.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode past=null;
+        ListNode curr=head;
+        ListNode next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=past;
+            past=curr;
+            curr=next;
+        }
+        head=past;
+        return head;
+
+        
+    }
+}
