e9fe7ed223ebbdc6cfecd15b5b65b792dac0411a
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-05 15:08:41 +0530

    Add files via upload

---
+++ MiddleLinkedList2.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+       int count=0;
+       ListNode curr=head;
+       while(curr!=null)
+       {
+        count++;
+        curr=curr.next;
+       }
+       int half=count/2;
+       int i=0;
+       ListNode c=head;
+       while(i!=half)
+       {
+        c=c.next;
+        i++;
+       }
+       return c;
+    }
+}
