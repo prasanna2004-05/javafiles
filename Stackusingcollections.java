5b09b452fd23588e283cf793da6a70c3174c87be
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-05 15:15:33 +0530

    Add files via upload

---
+++ Stackusingcollections.java
@@ -0,0 +1,15 @@
+ import java.util.Stack;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(1);
+	    s.push(2);
+	     System.out.println(s);
+	    System.out.println(s.pop());
+	    System.out.println(s.peek());
+	    System.out.println(s);
+	    
+	}
+}
