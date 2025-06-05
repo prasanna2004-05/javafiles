909dcd496524fdc588f9276fe94dadf813891c9e
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-05 15:18:09 +0530

    Add files via upload

---
+++ Digits  or not.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String a=sc.nextLine();
+	    for(int i=0;i<a.length();i++){
+	        char ch=a.charAt(i);
+	       if(Character.isDigit(ch)==false){
+	           System.out.println("not digts");
+	           System.exit(0);
+	       } 
+	    }
+		System.out.println("digits");
+	}
+}
