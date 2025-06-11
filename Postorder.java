559a1396eaf0c3c2b05841a76117c7571672d1cb
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-11 20:05:08 +0530

    Add files via upload

---
+++ Postorder.java
@@ -0,0 +1,85 @@
+class node 
+{
+    int data;
+    node left;
+    node right;
+    node(int n)
+    {
+        this.data=n;
+        this.left=null;
+        this.right=null;
+    }
+}
+class tree 
+ {
+    node root;
+    tree()
+    {
+        this.root=null;
+    }
+    void insert(int n){
+        node newnode=new node(n);
+        if(this.root==null)
+        this.root=newnode;
+        else{
+            node curr=this.root;
+            while(true)
+            {
+                if(n>curr.data)
+                {
+                    if(curr.right==null)
+                    {
+                        curr.right=newnode;
+                        break;
+                    }
+                    else
+                    {
+                        curr=curr.right;
+                    }
+                }
+                else{
+                    if(curr.left==null)
+                    {
+                        curr.left=newnode;
+                        break;
+                    }
+                    else
+                    {
+                        curr=curr.left;
+                    
+                }
+            }
+        }
+    }
+}
+void inorder(node root)
+{
+    if(root!=null)
+    {
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+}
+
+
+void postorder(node root)
+{
+    if(root!=null)
+    {
+        inorder(root.left);
+        inorder(root.right);
+        System.out.println(root.data);
+    }
+}
+}
+public class Postorder
+{
+	public static void main(String[] args) {
+	   tree t=new tree();
+	   t.insert(6);
+	   t.insert(9);
+	   t.insert(5);
+	   t.postorder(t.root);
+	}
+}
