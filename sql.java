d44a8146caa0e9836ae168d476227791def2e338
prasanna2004-05 <sulochana022p@gmail.com>
2025-06-17 03:11:40 -0700

    Add files via upload

---
+++ sql.java
@@ -0,0 +1,7 @@
+create table student(
+id int,
+rollno int,
+name varchar(50)
+);
+insert into student(id,rollno,name) values(1,21,"arjun"),(2,22,"karan"),(3,23,"akarsh"),(4,25,"reddy");
+select*from student;
